e299043c2a6df8859f229fe9f5e5601246b494f1
gizmo49 <chikacheezy@gmail.com>
2019-10-15 16:35:33 +0100

    feat: home page done

---
+++ src/components/base/flowerback/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import flowerImg from "../../../assests/img/flowers-back.svg"
+import "./index.css";
+
+const FlowerBack = () => (
+    <div className="full--flower">
+        <img src={flowerImg} alt="   " />
+    </div>
+)
+
+export default FlowerBack;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import IntlTelInput from 'react-intl-tel-input';
 import { Link } from "react-router-dom";
+import IntlTelInput from 'react-intl-tel-input';
 import 'react-intl-tel-input/dist/main.css';
 import SweetCard from "../reuse/sweetcard";
 import SweetHead from "../reuse/sweetcard/sweetHead";
 import TwitterBird from "../../assests/img/twitterbird.svg"
+import FlowerBack from "../base/flowerback";
 
 
 export default class Home extends Component {
@@ -13,14 +14,13 @@ export default class Home extends Component {
         return (
             <section className="h-100">
                 <div className="row justify-content-center align-items-center h-100">
-                    <div className="col-md-6">
-
+                    <div className="col-xl-4 col-lg-6 col-md-6 col-sm-7 col-10">
+                        <FlowerBack />
                         <SweetCard>
-
                             <SweetHead title="Get Started"
                                 paragraph="Ut enim ad minim veniam, quis nostrud exercitation." />
 
-                            <div className="px-40">
+                            <div className="px-40 py-3">
                                 <div className="mb-3">
                                     <IntlTelInput preferredCountries={['NG']}
                                         inputClassName="form-control primary"
@@ -34,22 +34,20 @@ export default class Home extends Component {
 
                                 <button className="btn btn-primary">Sign up</button>
 
-                                <p className="text-center">
+                                <p className="text-center already">
                                     Already have an account?
-                                    <Link to="/login">login</Link>
+                                    <Link to="/login">Log in</Link>
                                 </p>
 
                                 <div className="d-flex justify-content-center">
-                                    <h6 class="tally"><span>or</span></h6>
+                                    <h6 className="tally"><span>or</span></h6>
                                 </div>
 
                                 <a href="#!" className="btn btn-primary twitter-link">
                                     <img src={TwitterBird} alt="twt" />
                                     <span>continue with twitter</span>
                                 </a>
-
                             </div>
-
                         </SweetCard>
 
                     </div>
